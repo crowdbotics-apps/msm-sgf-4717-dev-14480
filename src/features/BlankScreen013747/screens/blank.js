a0fd0fff55ab9e00e2e413ba2299e04c2eed57d5
Crowdbotics <team@crowdbotics.com>
2020-11-03 07:00:46 +0000

    Update components

--- src/features/BlankScreen013747/screens/blank.js
+++ src/features/BlankScreen013747/screens/blank.js
@@ -23,49 +23,9 @@ export default class Blank extends React.Component {
     }
   }
 
-  state = {
-    TextInput_3: "",
-    TextInput_4: "",
-    TextInput_5: "",
-    TextInput_6: "",
-    TextInput_7: "",
-    TextInput_8: ""
-  }
+  state = {}
 
-  render = () => (
-    <View>
-      <TextInput
-        placeholder="Sample text input placeholder"
-        value={this.state.TextInput_3}
-        onChangeText={nextValue => this.setState({ TextInput_3: nextValue })}
-      />
-      <TextInput
-        placeholder="Sample text input placeholder"
-        value={this.state.TextInput_4}
-        onChangeText={nextValue => this.setState({ TextInput_4: nextValue })}
-      />
-      <TextInput
-        placeholder="Sample text input placeholder"
-        value={this.state.TextInput_5}
-        onChangeText={nextValue => this.setState({ TextInput_5: nextValue })}
-      />
-      <TextInput
-        placeholder="Sample text input placeholder"
-        value={this.state.TextInput_6}
-        onChangeText={nextValue => this.setState({ TextInput_6: nextValue })}
-      />
-      <TextInput
-        placeholder="Sample text input placeholder"
-        value={this.state.TextInput_7}
-        onChangeText={nextValue => this.setState({ TextInput_7: nextValue })}
-      />
-      <TextInput
-        placeholder="Sample text input placeholder"
-        value={this.state.TextInput_8}
-        onChangeText={nextValue => this.setState({ TextInput_8: nextValue })}
-      />
-    </View>
-  )
+  render = () => <View />
 }
 
 const styles = StyleSheet.create({
@@ -82,5 +42,6 @@ const styles = StyleSheet.create({
   TextInput_5: {},
   TextInput_6: {},
   TextInput_7: {},
-  TextInput_8: {}
+  TextInput_8: {},
+  View_1: {}
 })
